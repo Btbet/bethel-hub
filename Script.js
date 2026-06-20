068c271151ee4a5cceded6fbdb8247521e074edb
DaveMindX <ogabety20@gmail.com>
2026-06-20 03:33:36 +0100

    Add event listener for learnBtn click to scroll

--- Script.js
+++ Script.js
@@ -1,9 +1,10 @@
+
 <script>
-function goScroll() {
+document.getElementById("learnBtn").addEventListener("click", function () {
   alert("Keep Scrolling, Glad to have you...");
 
   document.getElementById("about").scrollIntoView({
     behavior: "smooth"
   });
-}
+});
 </script>
