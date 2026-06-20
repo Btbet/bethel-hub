1c2652ad3b11f9074cd5073ab4cf3ccc29d4f42d
DaveMindX <ogabety20@gmail.com>
2026-06-20 20:47:19 +0100

    Update submit.js

--- functions/submit.js
+++ functions/submit.js
@@ -1,1 +1,13 @@
 
+export async function onRequestPost(context) {
+  const { request, env } = context;
+
+  const data = await request.json();
+
+  await env.DB
+    .prepare("INSERT INTO submissions (name) VALUES (?)")
+    .bind(data.name)
+    .run();
+
+  return new Response("Saved successfully");
+}
